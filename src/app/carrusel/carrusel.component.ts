55217d521a9fb730210178722d15be9740ee101a
JosephAntony37900 <Joseantoniopintoaguilar@gmail.com>
2024-09-25 06:47:08 -0600

    Cards de estrenos acomodadas correctamente

--- src/app/carrusel/carrusel.component.ts
+++ src/app/carrusel/carrusel.component.ts
@@ -10,14 +10,22 @@ export class CarruselComponent {
   animes = [
     { title: 'Re:Zero', img: 'assets/carrusel/re.jpeg' },
     { title: 'White-haired Warrior', img: 'assets/carrusel/bell.jpg' },
-    { title: 'Tokyo Ghoul', img: 'assets/carrusel/GHOUL.jpeg' }
+    { title: 'Tokyo Ghoul', img: 'assets/carrusel/GHOUL.jpeg' },
+    {title: 'kekkai sense', img: 'assets/carrusel/kekkai.jpeg'},
+    {title: 'solo leveling', img: 'assets/carrusel/solo-leveling-vol-02-gn-manga.jpg'}
   ];
 
   goNext() {
-    this.currentIndex = (this.currentIndex + 1) % this.animes.length;
+    this.currentIndex = (this.currentIndex + 4) % this.animes.length;
   }
-
+  
   goPrevious() {
-    this.currentIndex = (this.currentIndex - 1 + this.animes.length) % this.animes.length;
+    this.currentIndex = (this.currentIndex - 4 + this.animes.length) % this.animes.length;
   }
+  
+
+  getVisibleAnimes() {
+    return this.animes.slice(this.currentIndex, this.currentIndex + 4);
+  }
+  
 }
